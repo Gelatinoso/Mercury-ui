7bce3495740326c506e221b26060a743b50034cd
Ivan Guzman <angel-g-ch@hotmail.com>
2021-06-05 02:30:22 -0600

    more legible code

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 local nuklear = require "nuklear"
 local inspect = require "inspect"
 local mercury = require "mercury"
+local packages = require "windows.packages"
+local popup = require "windows.popup"
 
 
 local ui
@@ -15,64 +17,13 @@ function love.load()
     ui = nuklear.newUI()
 end
 
--- State used to reflect data on the rendering
-local searchFilter = {value = "", placeholder = "Search..."}
-local packages = mercury.fetchPackages() or {}
-local title = "List of all packages available on the Mercury repository."
-local canInstall = true
 
 function love.update(dt)
-    local windowWidth = love.graphics.getWidth()
-    local windowHeight = love.graphics.getHeight()
     -- LuaFormatter off
     ui:frameBegin()
-        if ui:windowBegin("mercury", 0, 0, windowWidth, windowHeight, "border") then
-            local x, y, width, height = ui:windowGetContentRegion()
-            -- Tabs bar
-            ui:menubarBegin()
-                ui:layoutRow("dynamic", 30, 4)
-                if ui:button("Available") then
-                    canInstall = true
-                    title = "List of all packages available on the Mercury repository."
-                    searchFilter.value = ""
-                        packages = mercury.fetchPackages()
-                    end
-                    if ui:button("Installed") then
-                        title = "List of all the packages that already installed on the game."
-                        searchFilter.value = ""
-                        packages = mercury.getInstalled()
-                        canInstall = false
-                    end
-                ui:spacing(1)
-                ui:edit("simple", searchFilter)
-                height = height - 30
-            ui:menubarEnd()
-            -- Packages list
-            ui:layoutRow("dynamic", height, 1)
-                ui:groupBegin("packages", "scrollbar")
-                    ui:layoutRow("dynamic", 30, 1)
-                    ui:label(title)
-                    -- Show packages loaded in memory
-                    ui:layoutRow("dynamic", height / 3, 3)
-                    local packagesInRow = 0
-                    for packageIndex, package in pairs(packages) do
-                        if (package.label:find(searchFilter.value)) then
-                            packagesInRow = packagesInRow + 1
-                            if ui:button(package.label)then
-                                if canInstall then
-                                    response = mercury.installPackage(package.label)
-                                    print(response)
-                                end
-                            end
-                            if packagesInRow == 3 then
-                                ui:layoutRow("dynamic", height / 3, 3)
-                            end
-                        end
-                    end
-                ui:groupEnd()
-                
-        end
-        ui:windowEnd()
+        packages(ui)
+        -- TODO: make the popup show correctly
+        --popup(ui)
     ui:frameEnd()
     -- LuaFormatter on
 end
